0f1cc61d31e900ecb49cfdacffdbc758b2a1b5fa
Juana Llanje <juana.lc@hotmail.es>
2022-07-24 12:38:41 -0500

    Doing class number one of Vue

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Botas', // Sintaxis de JS y la info que arroja la erás en el HTML
+            description: 'Un cálido par de calcetines difusos.'
+        }
+    }
+})
+// Montando App
+const mountedApp = app.mount('#app') // Permite conectar con el app del DOM
