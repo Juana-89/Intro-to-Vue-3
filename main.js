8f1a77efeb43fa61513f90d9f4650341b527b35b
Juana Llanje <juana.lc@hotmail.es>
2022-07-24 22:36:07 -0500

    Doing class number six of Vue

--- main.js
+++ main.js
@@ -1,35 +1,51 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart:0,
             product: 'Botas', // Sintaxis de JS y la info que arroja la verás en el HTML
+            brand: 'Vue Mastery',
             description: 'Un cálido par de calcetines difusos.', // Se mostrará este texto
-            image: './assets/images/socks_blue.jpg',
-            url: 'https://www.vuemastery.com/',
-            // inStock: true Se mostrará dependiendo de si hay stock o no
-            inventory: 100, // Se mostrará 'in Stock' si inventory es mayor a 10
-            inStock: false,
+            selectedVariant: 0,
             details: ['50% algodón', '30% lana', '20% poliéster'],
             sizes: ['S', 'M', 'L', 'XL'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
               ],
-            cart:0
+            onSale: true
         }
     },
     methods: {
         addToCart() {
             this.cart += 1 // Llamar al método 'addToCart' para que vaya sumando 1 
         },
-        updateImage(variantImage) {
-            this.image = variantImage // Llamar al método 'updateImage' para actualizar la imagen cuando el puntero esté por encima del 'color' 
-          },
         removeToCart() {
             if(this.cart >=1) {
             this.cart -= 1 // Llamar al método 'removeToCart' para que vaya restando 1 
             }
         },
-      }
+        updateVariant(index) {
+            this.selectedVariant = index
+        }
+      },
+      computed: {
+        title() {
+          return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        saleMessage() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' is on sale.'
+            }
+            return ''
+        }
+      },
+      
 })
 // Montando App
 const mountedApp = app.mount('#app') // Permite conectar con el app del DOM
